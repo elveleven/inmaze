102fe644ae54dd4433514894cfb9e5865146f6f2
elveleven <rudenkoeleven@email.com>
2026-02-19 01:18:30 +0200

    fix: remove hybrid output for astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'hybrid',
   adapter: node({
     mode: 'standalone'
   }),
